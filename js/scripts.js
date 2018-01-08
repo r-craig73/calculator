066ed0b4a3ac4a1e9d5551c9f0eae27dc0c704f9
Hamza Naeem and Ron Craig <ron@>
2018-01-08 13:17:01 -0800

    initial setup

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var add = function(number1, number2) {return number1 + number2;
+};
+
+var minus = function(number1, number2) {return number1 - number2;
+};
+
+var multiply = function(number1, number2) {return number1 * number2;
+};
+
+var divide = function(number1, number2) {return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
