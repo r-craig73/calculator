46d867cf39d0ac9b9248a44de7bbb923054c43b7
Ron Craig <ron.craig@comcast.net>
2018-08-23 21:22:01 -0700

    edit code indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
-var add = function(number1, number2) {return number1 + number2;
-};
+var add = function(number1, number2) {return number1 + number2;};
 
-var minus = function(number1, number2) {return number1 - number2;
-};
+var minus = function(number1, number2) {return number1 - number2;};
 
-var multiply = function(number1, number2) {return number1 * number2;
-};
+var multiply = function(number1, number2) {return number1 * number2;};
 
-var divide = function(number1, number2) {return number1 / number2;
-};
+var divide = function(number1, number2) {return number1 / number2;};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
