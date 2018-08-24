d201db45dcaf9a868f0dbc83991abefd9e297cdb
Ron Craig <ron.craig@comcast.net>
2018-08-23 21:35:00 -0700

    edit js notation and insert all four function alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-var add = function(number1, number2) {return number1 + number2;};
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-var minus = function(number1, number2) {return number1 - number2;};
+var minus = function(number1, number2) {
+  return number1 - number2;
+};
 
-var multiply = function(number1, number2) {return number1 * number2;};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-var divide = function(number1, number2) {return number1 / number2;};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+alert("The sum is " + add(number1, number2) + ".");
+
+alert("The difference is " + minus(number1, number2) + ".");
+
+alert("The product is " + multiply(number1, number2) + ".");
+
+alert("The ratio is " + divide(number1, number2) + ".");
